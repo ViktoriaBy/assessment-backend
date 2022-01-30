af71af59950dffd78538a813bc1fd76b66640040
ViktoriaB <v.chamborant@hotmail.com>
2022-01-30 00:42:33 -0600

    Edited

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
 
+const app = express();
+const {getDogs, createDog, deleteDog, updateDog} = require('./controller')
+const port = 4000
 
 app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
+
 
 app.get("/api/compliment", (req, res) => {
   const compliments = ["A beautiful, smart, and loving person will be coming into your life",
@@ -27,36 +30,19 @@ app.get("/api/compliment", (req, res) => {
   res.status(200).send(randomCompliment);
   
 });
-// let characters
-let users = []
-
-app.get('/users', (req, res) => {
-  res.status(200).send(users)
-})
-
-app.get('/users/:name', (req, res) => {
-  const { name } = req.params
-  
-  res.status(200).send(users[index])
-})
 
 
 
-let id = []
-
-app.post('/users', (req, res) => {
-  let newUser = {...req.body, id}
-  users.unshift(newUser)
-  res.status(200).send(users)
-  id++
-})
-
- app.delete('/users/:name', (req, res)=>{
-  
-  res.status(400).send("User not found.")
-})
- 
+app.get(`/api/dogs`, getDogs)
+app.post(`/api/dogs`, createDog)
+app.put(`/api/dogs/:id`, updateDog)
+app.delete(`/api/dogs/:id`, deleteDog)
 
 
 
-app.listen(4000, () => console.log("Server running on 4000"));
+
+
+
+
+
+app.listen(port, () => console.log("Server running on 4000"));
